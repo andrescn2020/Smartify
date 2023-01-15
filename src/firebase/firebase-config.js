383a0c076fb74db710aea92c48a8096a121c1164
Andres <andrescn_2010@hotmail.com>
2023-01-15 17:57:55 -0300

    new firebase credentials

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -11,18 +11,32 @@ import { getAuth } from "firebase/auth";
 
 
 
+// const firebaseConfig = {
+
+//   apiKey: "AIzaSyD9uveHHol4bMl3DviVhDgoXe48rIuHPkA",
+//   authDomain: "e-commerce-dcfb1.firebaseapp.com",
+//   projectId: "e-commerce-dcfb1",
+//   storageBucket: "e-commerce-dcfb1.appspot.com",
+//   messagingSenderId: "16952118469",
+//   appId: "1:16952118469:web:5cb26f457afcded57d74ac"
+
+// };
+
 const firebaseConfig = {
 
-  apiKey: "AIzaSyD9uveHHol4bMl3DviVhDgoXe48rIuHPkA",
-  authDomain: "e-commerce-dcfb1.firebaseapp.com",
-  projectId: "e-commerce-dcfb1",
-  storageBucket: "e-commerce-dcfb1.appspot.com",
-  messagingSenderId: "16952118469",
-  appId: "1:16952118469:web:5cb26f457afcded57d74ac"
+  apiKey: "AIzaSyCOk5dm4P3TKXeRUuf-ACCfgqO2RFiOOsY",
+
+  authDomain: "e-commerce-63f6d.firebaseapp.com",
+
+  projectId: "e-commerce-63f6d",
+
+  storageBucket: "e-commerce-63f6d.appspot.com",
+
+  messagingSenderId: "813581752420",
+
+  appId: "1:813581752420:web:28717dd78ae72cde05e771"
 
 };
-
-
 
 
 // Initialize Firebase
